75b42eee84efa6e9461c71445bb29c4402dbb310
Pingorin <125970228+Pingorin@users.noreply.github.com>
2025-11-17 00:48:13 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, filters
+from pyrogram import Client, filters, enums
 from info import CHANNELS
 from database.ia_filterdb import save_file
 
@@ -7,7 +7,17 @@ media_filter = filters.document | filters.video
 @Client.on_message(filters.chat(CHANNELS) & media_filter)
 async def media(bot, message):
     media = getattr(message, message.media.value, None)
-    if media.mime_type in ['video/mp4', 'video/x-matroska']:  # Non .mp4 and .mkv files are skipped
+    
+    # Check karein ki media 'None' nahi hai
+    if media and media.mime_type in ['video/mp4', 'video/x-matroska']:
         media.file_type = message.media.value
         media.caption = message.caption
-        await save_file(media)
+        
+        # --- YEH HAI FIX ---
+        # File save karne se pehle uska address (ID) batayein
+        media.message_id = message.id
+        media.channel_id = message.chat.id
+        # --- FIX KHATAM ---
+
+        # 'secondary' (default) DB mein save karega
+        await save_file(media, 'secondary') 
