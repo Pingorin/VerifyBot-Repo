66a91680b2bc11aa8981d238cba9ea7f542a525a
Pingorin <125970228+Pingorin@users.noreply.github.com>
2025-11-16 11:23:25 +0530

    Create __init__.py

---
+++ plugins/__init__.py
@@ -0,0 +1,31 @@
+from aiohttp import web
+from .route import routes
+from asyncio import sleep 
+from datetime import datetime
+from database.users_chats_db import db
+from info import LOG_CHANNEL
+
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
+
+async def check_expired_premium(client):
+    while 1:
+        data = await db.get_expired(datetime.now())
+        for user in data:
+            user_id = user["id"]
+            await db.remove_premium_access(user_id)
+            try:
+                user = await client.get_users(user_id)
+                await client.send_message(
+                    chat_id=user_id,
+                    text=f"<b>ʜᴇʏ {user.mention},\n\nʏᴏᴜʀ ᴘʀᴇᴍɪᴜᴍ ᴀᴄᴄᴇss ʜᴀs ᴇxᴘɪʀᴇᴅ, ᴛʜᴀɴᴋ ʏᴏᴜ ꜰᴏʀ ᴜsɪɴɢ ᴏᴜʀ sᴇʀᴠɪᴄᴇ 😊\n\nɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴛᴀᴋᴇ ᴛʜᴇ ᴘʀᴇᴍɪᴜᴍ ᴀɢᴀɪɴ, ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ /plan ꜰᴏʀ ᴛʜᴇ ᴅᴇᴛᴀɪʟs ᴏꜰ ᴛʜᴇ ᴘʟᴀɴs...</b>"
+                )
+                await client.send_message(LOG_CHANNEL, text=f"<b>#Premium_Expire\n\nUser name: {user.mention}\nUser id: <code>{user_id}</code>")
+            except Exception as e:
+                print(e)
+            await sleep(0.5) # User ko message bhejte waqt rate limit se bachne ke liye
+        
+        # FIX: Database ko har second check karna zaroori nahi hai. 5 minute (300 sec) kaafi hai.
+        await sleep(300)
